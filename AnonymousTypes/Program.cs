1fc7499ca170463301a681f247062c0b941ce248
haraonline <haragopal.kovvali@gmail.com>
2023-06-19 11:25:32 +0200

    anonymous type

--- AnonymousTypes/Program.cs
+++ AnonymousTypes/Program.cs
@@ -1,11 +1,27 @@
-﻿namespace AnonymousTypes
+﻿using System.Reflection.PortableExecutable;
+
+namespace AnonymousTypes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var newCar = new { Color = "Red", Make = "BMW", Speed = 55 };
-            Console.WriteLine("You have a {0} {1} going {2} MPH", newCar.Color, newCar.Make, newCar.Speed);
+            var newCar = new
+            {
+                Color = "Red",
+                Make = "BMW",
+                Speed = 55,
+                isNew = true,
+                features = new string[] { "Airbags", "ABS", "GPS" }
+            };
+
+            //write all the values in one line to console. Join the features array with comma
+            Console.WriteLine($"Color: {newCar.Color}, Make: {newCar.Make}, Speed: {newCar.Speed}, isNew: {newCar.isNew}," +
+                $" features: {string.Join(",", newCar.features)}");
+
+            
+
+            
         }
     }
 }
