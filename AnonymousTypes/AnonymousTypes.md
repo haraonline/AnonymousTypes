208caaaec36d7b037a97151fb31e64a57af7338c
haraonline <haragopal.kovvali@gmail.com>
2023-06-19 11:17:17 +0200

    anonymous types in C# example

---
+++ AnonymousTypes/AnonymousTypes.md
@@ -0,0 +1,2 @@
+﻿https://chat.openai.com/share/94983ebd-edaf-484a-9a9e-e09efc2a977e
+
--- AnonymousTypes/Program.cs
+++ AnonymousTypes/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var newCar = new { Color = "Red", Make = "BMW", Speed = 55 };
+            Console.WriteLine("You have a {0} {1} going {2} MPH", newCar.Color, newCar.Make, newCar.Speed);
         }
     }
 }
